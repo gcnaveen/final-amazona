db300ed26286dbcc51ee9ffad02f64f8c486b852
gcnaveen <93068087+gcnaveen@users.noreply.github.com>
2023-01-24 13:11:51 +0530

    add

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.get('/api/keys/google', (req, res) => {
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
